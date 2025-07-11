798f9c6a2b874375aeefcde4484cf50902fec006
Fernando <fernando@juandemariana.org>
2025-07-11 12:23:41 +0200

    Add AutonomousCommunity model and migration.

---
+++ app/Models/AutonomousCommunity.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class AutonomousCommunity extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'code',
+        'country_id',
+        'latitude',
+        'longitude',
+        'area_km2',
+        'altitude_m',
+        'timezone_id',
+    ];
+
+    public function country()
+    {
+        return $this->belongsTo(Country::class);
+    }
+
+    public function timezone()
+    {
+        return $this->belongsTo(Timezone::class);
+    }
+
+    public function provinces()
+    {
+        return $this->hasMany(Province::class);
+    }
+}
