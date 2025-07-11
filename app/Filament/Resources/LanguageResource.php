229254a224545bbc96d09377191af559a66c6155
Fernando <fernando@juandemariana.org>
2025-07-11 14:02:39 +0200

    Add Language Filament resource.

---
+++ app/Filament/Resources/LanguageResource.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\LanguageResource\Pages;
+use App\Filament\Resources\LanguageResource\RelationManagers;
+use App\Models\Language;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+
+class LanguageResource extends Resource
+{
+    protected static ?string $model = Language::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-language';
+    protected static ?string $navigationGroup = 'Ubicaciones';
+
+
+    public static function form(Form $form): Form
+    {
+        return $form->schema([
+            TextInput::make('language')->required()->maxLength(100),
+            TextInput::make('slug')->required()->maxLength(100),
+            TextInput::make('native_name')->maxLength(100),
+            TextInput::make('iso_639_1')->maxLength(10),
+            TextInput::make('iso_639_2')->maxLength(10),
+            Toggle::make('rtl')->label('Es RTL'),
+        ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table->columns([
+            TextColumn::make('language')->sortable()->searchable(),
+            TextColumn::make('iso_639_1')->label('ISO 639-1'),
+            TextColumn::make('native_name'),
+            IconColumn::make('rtl')->label('RTL')->boolean(),
+        ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListLanguages::route('/'),
+            'create' => Pages\CreateLanguage::route('/create'),
+            'edit' => Pages\EditLanguage::route('/{record}/edit'),
+        ];
+    }
+}
