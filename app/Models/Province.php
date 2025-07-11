8aa8da8389f7bb86a568e1921327e572b22d9cc4
Fernando <fernando@juandemariana.org>
2025-07-11 12:26:07 +0200

    Add Province model and migration.

---
+++ app/Models/Province.php
@@ -0,0 +1,43 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Province extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'ine_code',
+        'autonomous_community_id',
+        'country_id',
+        'latitude',
+        'longitude',
+        'area_km2',
+        'altitude_m',
+        'timezone_id',
+    ];
+
+    public function autonomousCommunity()
+    {
+        return $this->belongsTo(AutonomousCommunity::class);
+    }
+
+    public function country()
+    {
+        return $this->belongsTo(Country::class);
+    }
+
+    public function regions()
+    {
+        return $this->hasMany(Region::class);
+    }
+
+    public function timezone()
+    {
+        return $this->belongsTo(Timezone::class);
+    }
+}
