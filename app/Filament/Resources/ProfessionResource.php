ec245a099e2d0d78e7257e588ffa3bd0a5906fe9
Fernando <fernando@juandemariana.org>
2025-07-12 07:12:19 +0200

    Add Profession Filament resource

---
+++ app/Filament/Resources/ProfessionResource.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ProfessionResource\Pages;
+use App\Filament\Resources\ProfessionResource\RelationManagers;
+use App\Models\Profession;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ProfessionResource extends Resource
+{
+    protected static ?string $model = Profession::class;
+
+    protected static ?string $navigationGroup = 'People';
+    protected static ?string $navigationIcon = 'heroicon-o-briefcase';
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')->required(),
+                Forms\Components\TextInput::make('slug')->required()->unique(ignoreRecord: true),
+                Forms\Components\TextInput::make('category')->required(),
+                Forms\Components\Toggle::make('is_public_facing')->default(true),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('category'),
+                Tables\Columns\IconColumn::make('is_public_facing')->boolean(),
+            ])
+            ->filters([])
+            ->defaultSort('name');
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProfessions::route('/'),
+            'create' => Pages\CreateProfession::route('/create'),
+            'edit' => Pages\EditProfession::route('/{record}/edit'),
+        ];
+    }
+}
