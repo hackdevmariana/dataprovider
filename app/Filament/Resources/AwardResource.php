d68d79736251d862537f0f86bdcd100c2efbc6e0
Fernando <fernando@juandemariana.org>
2025-07-12 09:21:19 +0200

    Add Award Filament resource.

---
+++ app/Filament/Resources/AwardResource.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\AwardResource\Pages;
+use App\Filament\Resources\AwardResource\RelationManagers;
+use App\Models\Award;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class AwardResource extends Resource
+{
+    protected static ?string $model = Award::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-trophy';
+    protected static ?string $navigationGroup = 'People';
+
+    public static function form(Form $form): Form
+    {
+        return $form->schema([
+            Forms\Components\TextInput::make('name')->required()->maxLength(255),
+            Forms\Components\TextInput::make('slug')->required()->unique(ignoreRecord: true),
+            Forms\Components\Textarea::make('description')->maxLength(1000),
+            Forms\Components\TextInput::make('awarded_by')->maxLength(255),
+            Forms\Components\TextInput::make('category')->maxLength(255),
+            Forms\Components\TextInput::make('first_year_awarded')->numeric()->minValue(0),
+        ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')->searchable(),
+                Tables\Columns\TextColumn::make('category'),
+                Tables\Columns\TextColumn::make('awarded_by'),
+                Tables\Columns\TextColumn::make('first_year_awarded'),
+            ])
+            ->filters([])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListAwards::route('/'),
+            'create' => Pages\CreateAward::route('/create'),
+            'edit' => Pages\EditAward::route('/{record}/edit'),
+        ];
+    }
+}
