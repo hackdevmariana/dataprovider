1ed196dec02c9a1ac1fd0e833bd42258f09e72f5
Fernando <fernando@juandemariana.org>
2025-07-12 07:18:01 +0200

    Add Tag Filament resource.

---
+++ app/Filament/Resources/TagResource.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\TagResource\Pages;
+use App\Filament\Resources\TagResource\RelationManagers;
+use App\Models\Tag;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class TagResource extends Resource
+{
+    protected static ?string $model = Tag::class;
+
+    protected static ?string $navigationGroup = 'Tags';
+    protected static ?string $navigationIcon = 'heroicon-o-tag';
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')->required(),
+                Forms\Components\TextInput::make('slug')->required()->unique(ignoreRecord: true),
+                Forms\Components\Select::make('tag_type')->options([
+                    'topic' => 'Topic',
+                    'style' => 'Style',
+                    'theme' => 'Theme',
+                    'mood' => 'Mood',
+                    'santoral' => 'Santoral',
+                    'international_day' => 'International Day',
+                ])->required(),
+                Forms\Components\Toggle::make('is_searchable')->default(true),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')->searchable(),
+                Tables\Columns\TextColumn::make('tag_type'),
+                Tables\Columns\IconColumn::make('is_searchable')->boolean(),
+            ])
+            ->filters([])
+            ->defaultSort('name');
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTags::route('/'),
+            'create' => Pages\CreateTag::route('/create'),
+            'edit' => Pages\EditTag::route('/{record}/edit'),
+        ];
+    }
+}
