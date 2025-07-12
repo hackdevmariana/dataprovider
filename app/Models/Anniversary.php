13f128406f1e980ad743e47b143577a7d36f7a32
Fernando <fernando@juandemariana.org>
2025-07-12 19:58:34 +0200

    Insert code to Anniversary model.

--- app/Models/Anniversary.php
+++ app/Models/Anniversary.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
 class Anniversary extends Model
 {
-    //
+    protected $fillable = [
+        'day',
+        'year',
+        'title',
+        'slug',
+        'description',
+    ];
+
+    public function tags(): MorphToMany
+    {
+        return $this->morphToMany(Tag::class, 'taggable');
+    }
 }
