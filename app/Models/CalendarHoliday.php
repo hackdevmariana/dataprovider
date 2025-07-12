5d24ea828b98ac4d43851f08f9442f6774c6baa3
Fernando <fernando@juandemariana.org>
2025-07-12 19:59:24 +0200

    Insert code to CalendarHoliday model.

--- app/Models/CalendarHoliday.php
+++ app/Models/CalendarHoliday.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class CalendarHoliday extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'date',
+        'slug',
+        'municipality_id',
+    ];
+
+    public function locations()
+    {
+        return $this->hasMany(CalendarHolidayLocation::class);
+    }
 }
