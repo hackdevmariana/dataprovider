940147cff3bbbd651ae1e209be1929a259e220a9
Fernando <fernando@juandemariana.org>
2025-07-13 20:07:38 +0200

    Add EventType Filament resource.

---
+++ app/Filament/Resources/EventTypeResource.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\EventTypeResource\Pages;
+use App\Filament\Resources\EventTypeResource\RelationManagers;
+use App\Models\EventType;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class EventTypeResource extends Resource
+{
+    protected static ?string $model = EventType::class;
+    protected static ?string $navigationGroup = 'Eventos culturales';
+
+    protected static ?string $navigationIcon = 'heroicon-o-bookmark';
+    public static function form(Form $form): Form
+    {
+        return $form->schema([
+            Forms\Components\TextInput::make('name')->required(),
+            Forms\Components\TextInput::make('slug')->required()->unique(ignoreRecord: true),
+            Forms\Components\Textarea::make('description')->rows(3),
+        ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table->columns([
+            Tables\Columns\TextColumn::make('name')->sortable()->searchable(),
+            Tables\Columns\TextColumn::make('slug')->sortable(),
+        ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListEventTypes::route('/'),
+            'create' => Pages\CreateEventType::route('/create'),
+            'edit' => Pages\EditEventType::route('/{record}/edit'),
+        ];
+    }
+}
