7ce3623fe79a3db3e5763cdc0b505cc054571940
Fernando <fernando@juandemariana.org>
2025-07-13 04:51:32 +0200

    Add Venue model.

---
+++ app/Models/Venue.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Venue extends Model
+{
+    protected $fillable = [
+        'name', 'slug', 'address', 'municipality_id', 'latitude', 'longitude',
+        'capacity', 'description', 'venue_type', 'venue_status', 'is_verified',
+    ];
+
+    public function municipality()
+    {
+        return $this->belongsTo(Municipality::class);
+    }
+
+    public function events()
+    {
+        return $this->hasMany(Event::class);
+    }
+}
