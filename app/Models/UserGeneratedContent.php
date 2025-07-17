08cf9feb9653ac8ce9322379202fd47789b2844e
Fernando <fernando@juandemariana.org>
2025-07-17 21:23:42 +0200

    Add UserGeneratedContent model code.

--- app/Models/UserGeneratedContent.php
+++ app/Models/UserGeneratedContent.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserGeneratedContent extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'related_type',
+        'related_id',
+        'content_type',
+        'content',
+        'language',
+        'visibility',
+        'status',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function related()
+    {
+        return $this->morphTo();
+    }
 }
