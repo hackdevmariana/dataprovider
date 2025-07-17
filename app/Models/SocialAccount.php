3f7fbac3d4662b381caa10a19e83587e537a54dd
Fernando <fernando@juandemariana.org>
2025-07-17 21:26:46 +0200

    Add SocialAccount model code.

--- app/Models/SocialAccount.php
+++ app/Models/SocialAccount.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class SocialAccount extends Model
 {
-    //
+    protected $fillable = [
+        'person_id',
+        'platform',
+        'handle',
+        'url',
+    ];
+
+    public function person()
+    {
+        return $this->belongsTo(Person::class);
+    }
 }
