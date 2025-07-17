84c757bbd4bdd03cc160b528e3f5680da0398127
Fernando <fernando@juandemariana.org>
2025-07-17 21:39:28 +0200

    Add Apperance model code.

--- app/Models/Appearance.php
+++ app/Models/Appearance.php
@@ -1,10 +1,22 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Appearance extends Model
 {
-    //
+    protected $table = 'person_physical_profiles';
+
+    protected $fillable = [
+        'person_id',
+        'height_cm',
+        'weight_kg',
+        'body_type',
+    ];
+
+    public function person(): BelongsTo
+    {
+        return $this->belongsTo(Person::class);
+    }
 }
