c4c72a8a7dbda14d60cdb9e1f0a7df7870a52585
Fernando <fernando@juandemariana.org>
2025-07-17 21:21:52 +0200

    Add DataSource model code.

--- app/Models/DataSource.php
+++ app/Models/DataSource.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class DataSource extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'url',
+        'description',
+        'official',
+        'country_code',
+    ];
+
+    public function stats()
+    {
+        return $this->hasMany(Stat::class);
+    }
 }
