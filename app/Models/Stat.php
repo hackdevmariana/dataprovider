82b525fa7362688975019157d59b5341f06c40c3
Fernando <fernando@juandemariana.org>
2025-07-17 21:27:15 +0200

    Add Stat model code.

--- app/Models/Stat.php
+++ app/Models/Stat.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Stat extends Model
 {
-    //
+    protected $fillable = [
+        'subject_type',
+        'subject_id',
+        'value',
+        'unit',
+        'source_id',
+        'recorded_at',
+        'note',
+    ];
+
+    public function subject()
+    {
+        return $this->morphTo();
+    }
+
+    public function source()
+    {
+        return $this->belongsTo(DataSource::class, 'source_id');
+    }
 }
