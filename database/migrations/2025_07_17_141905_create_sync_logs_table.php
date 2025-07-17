d091f3a605c965ca72828dfc3f69e663443857f4
Fernando <fernando@juandemariana.org>
2025-07-17 16:21:53 +0200

    Add SyncLog model and migration.

---
+++ database/migrations/2025_07_17_141905_create_sync_logs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sync_logs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('data_source_id')->nullable()->constrained('data_sources');
+            $table->enum('status', ['success', 'failed']);
+            $table->timestamp('started_at');
+            $table->timestamp('finished_at')->nullable();
+            $table->integer('processed_items_count')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sync_logs');
+    }
+};
