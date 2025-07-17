7d7b14b1cbf7ba1fc5dae2d4051e4dc03edee40b
Fernando <fernando@juandemariana.org>
2025-07-17 21:37:59 +0200

    Add ScrapingSource model code.

--- app/Models/ScrapingSource.php
+++ app/Models/ScrapingSource.php
@@ -1,10 +1,22 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class ScrapingSource extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'url',
+        'type',
+        'source_type_description',
+        'frequency',
+        'last_scraped_at',
+        'is_active',
+    ];
+
+    protected $casts = [
+        'last_scraped_at' => 'datetime',
+        'is_active' => 'boolean',
+    ];
 }
