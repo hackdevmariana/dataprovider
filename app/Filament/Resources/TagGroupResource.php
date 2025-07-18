da9a48743e95728496faf5ff57f2df7da5b69689
Fernando <fernando@juandemariana.org>
2025-07-18 23:08:23 +0200

    Add TagGroup Filament resource.

---
+++ app/Filament/Resources/TagGroupResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\TagGroupResource\Pages;
+use App\Filament\Resources\TagGroupResource\RelationManagers;
+use App\Models\TagGroup;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Tables\Columns\TextColumn;
+
+class TagGroupResource extends Resource
+{
+    protected static ?string $model = TagGroup::class;
+    protected static ?string $navigationIcon = 'heroicon-o-tag';
+    protected static ?string $navigationGroup = 'Admin';
+    protected static ?int $navigationSort = 2;
+    public static function form(Form $form): Form
+    {
+        return $form->schema([
+            TextInput::make('name')
+                ->required()
+                ->maxLength(255),
+
+            Textarea::make('description')
+                ->rows(4)
+                ->maxLength(500)
+                ->nullable(),
+        ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table->columns([
+            TextColumn::make('id')->sortable(),
+            TextColumn::make('name')->searchable()->sortable(),
+            TextColumn::make('description')->limit(50)->wrap(),
+            TextColumn::make('created_at')->dateTime()->sortable(),
+        ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTagGroups::route('/'),
+            'create' => Pages\CreateTagGroup::route('/create'),
+            'edit' => Pages\EditTagGroup::route('/{record}/edit'),
+        ];
+    }
+}
