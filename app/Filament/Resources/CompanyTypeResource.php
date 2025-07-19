ee7c93ca4f2179beb06a9b8bd8413dd74900a3ae
Fernando <fernando@juandemariana.org>
2025-07-19 10:03:18 +0200

    Add CompanyType filament resource.

---
+++ app/Filament/Resources/CompanyTypeResource.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CompanyTypeResource\Pages;
+use App\Filament\Resources\CompanyTypeResource\RelationManagers;
+use App\Models\CompanyType;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CompanyTypeResource extends Resource
+{
+    protected static ?string $model = CompanyType::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-building-office';
+
+
+    public static function form(Form $form): Form
+    {
+        return $form->schema([
+            Forms\Components\TextInput::make('name')
+                ->required()
+                ->maxLength(100),
+            Forms\Components\TextInput::make('slug')
+                ->required()
+                ->maxLength(100),
+        ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table->columns([
+            Tables\Columns\TextColumn::make('name')->searchable()->sortable(),
+            Tables\Columns\TextColumn::make('slug')->sortable(),
+        ])->filters([])->actions([
+            Tables\Actions\EditAction::make(),
+        ])->bulkActions([
+            Tables\Actions\DeleteBulkAction::make(),
+        ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCompanyTypes::route('/'),
+            'create' => Pages\CreateCompanyType::route('/create'),
+            'edit' => Pages\EditCompanyType::route('/{record}/edit'),
+        ];
+    }
+}
