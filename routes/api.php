d7e867b3a7dd8abf8bacb6e8e0a8f86d600843c0
Fernando <fernando@juandemariana.org>
2025-07-24 12:24:50 +0200

    Add api/v1/provinces route.

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\AppSettingController;
+use App\Http\Controllers\Api\ProvinceController;
 
 Route::prefix('v1')->middleware('auth:sanctum')->group(function () {
     Route::apiResource('app-settings', AppSettingController::class)->only(['index', 'show']);
+});
+
+// Rutas públicas sin autenticación
+Route::prefix('v1')->group(function () {
+    Route::apiResource('provinces', ProvinceController::class)->only(['index', 'show']);
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
