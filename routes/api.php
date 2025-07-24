76aca90a97b3da61de6300720d7c4d81e2c77546
Fernando <fernando@juandemariana.org>
2025-07-24 12:39:43 +0200

    Add api/v1/countries route.

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\AppSettingController;
 use App\Http\Controllers\Api\ProvinceController;
+use App\Http\Controllers\Api\CountryController;
 
 Route::prefix('v1')->middleware('auth:sanctum')->group(function () {
     Route::apiResource('app-settings', AppSettingController::class)->only(['index', 'show']);
@@ -12,6 +13,9 @@ Route::prefix('v1')->middleware('auth:sanctum')->group(function () {
 // Rutas públicas sin autenticación
 Route::prefix('v1')->group(function () {
     Route::apiResource('provinces', ProvinceController::class)->only(['index', 'show']);
+    Route::get('/countries', [CountryController::class, 'index']);
+    Route::get('/countries/{idOrSlug}', [CountryController::class, 'show']);
+
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
