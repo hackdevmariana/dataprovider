ac2c6dd113705dea04caeba526fcbc457062337b
Fernando <fernando@juandemariana.org>
2025-08-01 23:21:47 +0200

    Add Region resource.

---
+++ app/Http/Resources/RegionResource.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class RegionResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'slug' => $this->slug,
+            'latitude' => $this->latitude,
+            'longitude' => $this->longitude,
+            'area_km2' => $this->area_km2,
+            'altitude_m' => $this->altitude_m,
+            'province' => $this->province?->name,
+            'autonomous_community' => $this->autonomousCommunity?->name,
+            'country' => $this->country?->name,
+            'timezone' => $this->timezone?->name,
+        ];
+    }
+}
