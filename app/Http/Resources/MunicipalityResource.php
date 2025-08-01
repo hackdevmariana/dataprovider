8083f660a2bd12adb949191b1986435cc1c9aff8
Fernando <fernando@juandemariana.org>
2025-08-01 23:06:24 +0200

    Add Municipality resource.

---
+++ app/Http/Resources/MunicipalityResource.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class MunicipalityResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'slug' => $this->slug,
+            'ine_code' => $this->ine_code,
+            'postal_code' => $this->postal_code,
+            'population' => $this->population,
+            'is_capital' => $this->is_capital,
+            'latitude' => $this->latitude,
+            'longitude' => $this->longitude,
+            'area_km2' => $this->area_km2,
+            'altitude_m' => $this->altitude_m,
+        ];
+    }
+}
