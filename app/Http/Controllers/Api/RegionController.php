2249db3e05fc0cd0727a242404215ee0c2792024
Fernando <fernando@juandemariana.org>
2025-08-01 23:33:31 +0200

    Fix Region controller with resource.

--- app/Http/Controllers/Api/RegionController.php
+++ app/Http/Controllers/Api/RegionController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Controllers\Api;
 
 use App\Models\Region;
@@ -6,36 +7,51 @@ use App\Models\Province;
 use App\Models\AutonomousCommunity;
 use App\Models\Country;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\RegionResource;
 use Illuminate\Http\Request;
 
 class RegionController extends Controller
 {
     public function index()
     {
-        return response()->json(Region::all());
+        return RegionResource::collection(Region::with(['province', 'autonomousCommunity', 'country', 'timezone'])->get());
     }
 
     public function show($idOrSlug)
     {
-        $region = Region::where('slug', $idOrSlug)->orWhere('id', $idOrSlug)->firstOrFail();
-        return response()->json($region);
+        $region = Region::with(['province', 'autonomousCommunity', 'country', 'timezone'])
+                        ->where('slug', $idOrSlug)
+                        ->orWhere('id', $idOrSlug)
+                        ->firstOrFail();
+
+        return new RegionResource($region);
     }
 
     public function byProvince($slug)
     {
         $province = Province::where('slug', $slug)->firstOrFail();
-        return response()->json($province->regions);
+        $regions = $province->regions()->with(['province', 'autonomousCommunity', 'country', 'timezone'])->get();
+
+        return RegionResource::collection($regions);
     }
 
     public function byAutonomousCommunity($slug)
     {
         $community = AutonomousCommunity::where('slug', $slug)->firstOrFail();
-        return response()->json($community->provinces->flatMap->regions);
+
+        $regions = Region::with(['province', 'autonomousCommunity', 'country', 'timezone'])
+                         ->where('autonomous_community_id', $community->id)
+                         ->get();
+
+        return RegionResource::collection($regions);
     }
 
     public function byCountry($slug)
     {
         $country = Country::where('slug', $slug)->firstOrFail();
-        return response()->json($country->regions);
+
+        $regions = $country->regions()->with(['province', 'autonomousCommunity', 'country', 'timezone'])->get();
+
+        return RegionResource::collection($regions);
     }
 }
