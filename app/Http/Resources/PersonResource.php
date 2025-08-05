0145b542a2203dec6b221bf91d0a64db9ea952e7
Fernando <fernando@juandemariana.org>
2025-08-05 11:18:45 +0200

    Add Apperance and Person resources.

--- app/Http/Resources/PersonResource.php
+++ app/Http/Resources/PersonResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\AppearanceResource;
 
 class PersonResource extends JsonResource
 {
@@ -62,6 +63,10 @@ class PersonResource extends JsonResource
                     'alt' => $this->image->alt ?? null,
                 ];
             }),
+
+            'appearance' => new AppearanceResource($this->whenLoaded('appearance')),
+
+
         ];
     }
 }
