6000d861c1417379096df678235c80e51e5ab909
Fernando <fernando@juandemariana.org>
2025-08-05 12:13:41 +0200

    Add Profession resource.

---
+++ app/Http/Resources/ProfessionResource.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProfessionResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'slug' => $this->slug,
+            'category' => $this->category,
+            'is_public_facing' => $this->is_public_facing,
+        ];
+    }
+}
