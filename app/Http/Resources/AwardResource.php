d69b3fc2bd1de5a6673ab15b709f03bb987ae29e
Fernando <fernando@juandemariana.org>
2025-08-07 23:54:15 +0200

    Add Award resource.

---
+++ app/Http/Resources/AwardResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AwardResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'slug' => $this->slug,
+            'description' => $this->description,
+            'awarded_by' => $this->awarded_by,
+            'first_year_awarded' => $this->first_year_awarded,
+            'category' => $this->category,
+            'award_winners' => AwardWinnerResource::collection($this->whenLoaded('awardWinners')),
+        ];
+    }
+}
