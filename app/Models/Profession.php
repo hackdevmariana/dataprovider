221b481cce810816241613ba5f37747fb13602db
Fernando <fernando@juandemariana.org>
2025-08-11 11:46:35 +0200

    Includes documentation on the Profession model.

--- app/Models/Profession.php
+++ app/Models/Profession.php
@@ -4,6 +4,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
+/**
+ * Class Profession
+ *
+ * Represents a profession or occupation.
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $slug
+ * @property string|null $category
+ * @property bool $is_public_facing
+ */
 class Profession extends Model
 {
     use HasFactory;
