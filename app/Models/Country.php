64840cdc0b82161970156d4133f66c14d3aac9d7
Fernando <fernando@juandemariana.org>
2025-08-11 11:45:27 +0200

    Includes documentation on the Country model.

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -5,6 +5,35 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
+/**
+ * Class Country
+ *
+ * Represents a country.
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $slug
+ * @property string $iso_alpha2
+ * @property string $iso_alpha3
+ * @property string $iso_numeric
+ * @property string|null $demonym
+ * @property string|null $official_language
+ * @property string|null $currency_code
+ * @property string|null $phone_code
+ * @property float|null $latitude
+ * @property float|null $longitude
+ * @property string|null $flag_url
+ * @property int|null $population
+ * @property float|null $gdp_usd
+ * @property string|null $region_group
+ * @property float|null $area_km2
+ * @property float|null $altitude_m
+ * @property int|null $timezone_id
+ *
+ * @property-read Timezone $timezone
+ * @property-read \Illuminate\Database\Eloquent\Collection|Language[] $languages
+ * @property-read \Illuminate\Database\Eloquent\Collection|Region[] $regions
+ */
 class Country extends Model
 {
     use HasFactory;
