e49dbed8dcadafe4c246b0af514e0b0a9156ecb0
Fernando <fernando@juandemariana.org>
2025-08-11 11:46:05 +0200

    Includes documentation on the Province model.

--- app/Models/Province.php
+++ app/Models/Province.php
@@ -5,6 +5,29 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
+/**
+ * Class Province
+ *
+ * Represents a province.
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $slug
+ * @property string|null $ine_code
+ * @property int|null $autonomous_community_id
+ * @property int|null $country_id
+ * @property float|null $latitude
+ * @property float|null $longitude
+ * @property float|null $area_km2
+ * @property float|null $altitude_m
+ * @property int|null $timezone_id
+ *
+ * @property-read AutonomousCommunity $autonomousCommunity
+ * @property-read Country $country
+ * @property-read \Illuminate\Database\Eloquent\Collection|Region[] $regions
+ * @property-read Timezone $timezone
+ * @property-read \Illuminate\Database\Eloquent\Collection|Municipality[] $municipalities
+ */
 class Province extends Model
 {
     use HasFactory;
