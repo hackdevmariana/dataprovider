f7dbc05e9abe41fd51e07329f246e83cb96e2937
Fernando <fernando@juandemariana.org>
2025-08-11 11:45:07 +0200

    Includes documentation on the AutonomousCommunity model.

--- app/Models/AutonomousCommunity.php
+++ app/Models/AutonomousCommunity.php
@@ -5,6 +5,27 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
+/**
+ * Class AutonomousCommunity
+ *
+ * Represents an autonomous community (Spain).
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $slug
+ * @property string|null $code
+ * @property int|null $country_id
+ * @property float|null $latitude
+ * @property float|null $longitude
+ * @property float|null $area_km2
+ * @property float|null $altitude_m
+ * @property int|null $timezone_id
+ *
+ * @property-read Country $country
+ * @property-read Timezone $timezone
+ * @property-read \Illuminate\Database\Eloquent\Collection|Province[] $provinces
+ * @property-read \Illuminate\Database\Eloquent\Collection|Region[] $regions
+ */
 class AutonomousCommunity extends Model
 {
     use HasFactory;
