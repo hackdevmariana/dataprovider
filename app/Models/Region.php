2281361f014ab46bcadc114b19457e030ff98c93
Fernando <fernando@juandemariana.org>
2025-08-11 11:46:51 +0200

    Includes documentation on the Region model.

--- app/Models/Region.php
+++ app/Models/Region.php
@@ -5,6 +5,28 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
+/**
+ * Class Region
+ *
+ * Represents a region within a country.
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $slug
+ * @property int|null $province_id
+ * @property int|null $autonomous_community_id
+ * @property int|null $country_id
+ * @property float|null $latitude
+ * @property float|null $longitude
+ * @property float|null $area_km2
+ * @property float|null $altitude_m
+ * @property int|null $timezone_id
+ *
+ * @property-read Province $province
+ * @property-read AutonomousCommunity $autonomousCommunity
+ * @property-read Country $country
+ * @property-read Timezone $timezone
+ */
 class Region extends Model
 {
     use HasFactory;
