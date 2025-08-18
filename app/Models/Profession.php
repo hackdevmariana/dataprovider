db406572572f61f61e81548a9ff7fa8bba8fbe48
Fernando <fernando@juandemariana.org>
2025-08-18 15:56:42 +0200

    Fix Profession model.

--- app/Models/Profession.php
+++ app/Models/Profession.php
@@ -25,4 +25,59 @@ class Profession extends Model
         'category',
         'is_public_facing',
     ];
+
+    protected $casts = [
+        'is_public_facing' => 'boolean',
+    ];
+
+    /**
+     * Personas que ejercen esta profesión.
+     */
+    public function people()
+    {
+        return $this->belongsToMany(Person::class, 'person_profession')
+                    ->withPivot([
+                        'start_year',
+                        'end_year',
+                        'is_primary',
+                        'is_current',
+                        'notes'
+                    ])
+                    ->withTimestamps();
+    }
+
+    /**
+     * Personas que ejercen actualmente esta profesión.
+     */
+    public function currentPeople()
+    {
+        return $this->belongsToMany(Person::class, 'person_profession')
+                    ->wherePivot('is_current', true)
+                    ->withPivot([
+                        'start_year',
+                        'end_year',
+                        'is_primary',
+                        'is_current',
+                        'notes'
+                    ])
+                    ->withTimestamps();
+    }
+
+    /**
+     * Personas para las que esta es su profesión principal.
+     */
+    public function primaryPeople()
+    {
+        return $this->belongsToMany(Person::class, 'person_profession')
+                    ->wherePivot('is_primary', true)
+                    ->wherePivot('is_current', true)
+                    ->withPivot([
+                        'start_year',
+                        'end_year',
+                        'is_primary',
+                        'is_current',
+                        'notes'
+                    ])
+                    ->withTimestamps();
+    }
 }
