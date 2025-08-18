ed140fdbaa52ed86eb642758072bc971031cf8fe
Fernando <fernando@juandemariana.org>
2025-08-18 13:44:33 +0200

    Fix Person model.

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
@@ -44,6 +45,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
  */
 class Person extends Model
 {
+    use HasFactory;
     protected $fillable = [
         'name',
         'birth_name',
