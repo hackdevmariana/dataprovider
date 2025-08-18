7612d78e771faf05b65b7cc32ac7f9287df4cbf3
Fernando <fernando@juandemariana.org>
2025-08-18 15:31:33 +0200

    Fix User model.

--- app/Models/User.php
+++ app/Models/User.php
@@ -60,4 +60,77 @@ class User extends Authenticatable
             ->withPivot(['role', 'joined_at', 'is_active'])
             ->withTimestamps();
     }
+
+    public function userDevices()
+    {
+        return $this->hasMany(\App\Models\UserDevice::class);
+    }
+
+    public function notificationSettings()
+    {
+        return $this->hasMany(\App\Models\NotificationSetting::class);
+    }
+
+    public function stats()
+    {
+        return $this->morphMany(\App\Models\Stat::class, 'subject');
+    }
+
+    public function achievements()
+    {
+        return $this->belongsToMany(\App\Models\Achievement::class, 'user_achievements')
+                    ->withPivot([
+                        'progress',
+                        'level',
+                        'is_completed',
+                        'completed_at',
+                        'metadata',
+                        'value_achieved',
+                        'points_earned',
+                        'is_notified'
+                    ])
+                    ->withTimestamps();
+    }
+
+    public function userAchievements()
+    {
+        return $this->hasMany(\App\Models\UserAchievement::class);
+    }
+
+    public function completedAchievements()
+    {
+        return $this->belongsToMany(\App\Models\Achievement::class, 'user_achievements')
+                    ->wherePivot('is_completed', true)
+                    ->withPivot(['completed_at', 'level', 'points_earned'])
+                    ->withTimestamps();
+    }
+
+    public function challenges()
+    {
+        return $this->belongsToMany(\App\Models\Challenge::class, 'user_challenges')
+                    ->withPivot([
+                        'status',
+                        'joined_at',
+                        'completed_at',
+                        'progress',
+                        'current_value',
+                        'ranking_position',
+                        'points_earned',
+                        'reward_earned'
+                    ])
+                    ->withTimestamps();
+    }
+
+    public function userChallenges()
+    {
+        return $this->hasMany(\App\Models\UserChallenge::class);
+    }
+
+    public function activeChallenges()
+    {
+        return $this->belongsToMany(\App\Models\Challenge::class, 'user_challenges')
+                    ->wherePivot('status', 'active')
+                    ->withPivot(['current_value', 'ranking_position'])
+                    ->withTimestamps();
+    }
 }
