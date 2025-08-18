b2e5bf0a7671af69c4979bad1c8cc4df7022f6cc
Fernando <fernando@juandemariana.org>
2025-08-18 15:56:56 +0200

    Fix Work model.

--- app/Models/Work.php
+++ app/Models/Work.php
@@ -48,4 +48,107 @@ class Work extends Model
     {
         return $this->belongsTo(Link::class);
     }
+
+    /**
+     * Personas que han participado en esta obra.
+     */
+    public function people()
+    {
+        return $this->belongsToMany(Person::class, 'person_work')
+                    ->withPivot([
+                        'role',
+                        'character_name',
+                        'credited_as',
+                        'billing_order',
+                        'contribution_pct',
+                        'is_primary',
+                        'notes'
+                    ])
+                    ->withTimestamps();
+    }
+
+    /**
+     * Protagonistas de la obra.
+     */
+    public function leads()
+    {
+        return $this->belongsToMany(Person::class, 'person_work')
+                    ->wherePivot('is_primary', true)
+                    ->withPivot([
+                        'role',
+                        'character_name',
+                        'credited_as',
+                        'billing_order'
+                    ])
+                    ->orderByPivot('billing_order')
+                    ->withTimestamps();
+    }
+
+    /**
+     * Cast completo ordenado por importancia.
+     */
+    public function cast()
+    {
+        return $this->belongsToMany(Person::class, 'person_work')
+                    ->wherePivot('role', 'actor')
+                    ->withPivot([
+                        'character_name',
+                        'credited_as',
+                        'billing_order'
+                    ])
+                    ->orderByPivot('billing_order')
+                    ->withTimestamps();
+    }
+
+    /**
+     * Directores de la obra.
+     */
+    public function directors()
+    {
+        return $this->belongsToMany(Person::class, 'person_work')
+                    ->wherePivot('role', 'director')
+                    ->withPivot([
+                        'credited_as',
+                        'contribution_pct'
+                    ])
+                    ->withTimestamps();
+    }
+
+    /**
+     * Autores/escritores de la obra.
+     */
+    public function writers()
+    {
+        return $this->belongsToMany(Person::class, 'person_work')
+                    ->wherePivotIn('role', ['author', 'writer', 'guionista', 'escritor'])
+                    ->withPivot([
+                        'credited_as',
+                        'contribution_pct'
+                    ])
+                    ->withTimestamps();
+    }
+
+    /**
+     * Tags relacionados con la obra.
+     */
+    public function tags()
+    {
+        return $this->morphToMany(Tag::class, 'taggable');
+    }
+
+    /**
+     * Links relacionados con la obra.
+     */
+    public function links()
+    {
+        return $this->morphMany(Link::class, 'related');
+    }
+
+    /**
+     * Eventos relacionados con la obra (estrenos, presentaciones, etc.).
+     */
+    public function events()
+    {
+        return $this->hasMany(Event::class);
+    }
 }
