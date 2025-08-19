821f406a7b1ef69e6b059a71009d517556f3b431
Fernando <fernando@juandemariana.org>
2025-08-19 18:32:50 +0200

    Add UserAchievement controller.

---
+++ app/Http/Controllers/Api/V1/UserAchievementController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\UserAchievement;
+use Illuminate\Http\Request;
+
+class UserAchievementController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(UserAchievement $userAchievement)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, UserAchievement $userAchievement)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(UserAchievement $userAchievement)
+    {
+        //
+    }
+}
