4468f599881aa3bc4f49aa638d0324c08ce62740
Fernando <fernando@juandemariana.org>
2025-08-19 18:37:54 +0200

    Add ZoneClimate controller.

---
+++ app/Http/Controllers/Api/V1/ZoneClimateController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\ZoneClimate;
+use Illuminate\Http\Request;
+
+class ZoneClimateController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ZoneClimate $zoneClimate)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ZoneClimate $zoneClimate)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ZoneClimate $zoneClimate)
+    {
+        //
+    }
+}
