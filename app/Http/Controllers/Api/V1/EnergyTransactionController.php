bc975505b23b98c61986abb572782964a673b0e2
Fernando <fernando@juandemariana.org>
2025-08-19 18:19:39 +0200

    Add EnergyTransaction controller.

---
+++ app/Http/Controllers/Api/V1/EnergyTransactionController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\EnergyTransaction;
+use Illuminate\Http\Request;
+
+class EnergyTransactionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(EnergyTransaction $energyTransaction)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, EnergyTransaction $energyTransaction)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(EnergyTransaction $energyTransaction)
+    {
+        //
+    }
+}
