fdffd0a571756eb6e4c21543ef56721fd036d4f9
Fernando <fernando@juandemariana.org>
2025-08-19 07:39:57 +0200

    Add CalendarHoliday controller.

---
+++ app/Http/Controllers/Api/V1/CalendarHolidayLocationController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\CalendarHolidayLocation;
+use Illuminate\Http\Request;
+
+class CalendarHolidayLocationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CalendarHolidayLocation $calendarHolidayLocation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, CalendarHolidayLocation $calendarHolidayLocation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CalendarHolidayLocation $calendarHolidayLocation)
+    {
+        //
+    }
+}
