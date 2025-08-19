ba95ece8346d4fe566116d899e7887b78f0df737
Fernando <fernando@juandemariana.org>
2025-08-19 07:38:30 +0200

    Add Appearance controller.

---
+++ app/Http/Controllers/Api/V1/AppearanceController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Appearance;
+use Illuminate\Http\Request;
+
+class AppearanceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Appearance $appearance)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Appearance $appearance)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Appearance $appearance)
+    {
+        //
+    }
+}
