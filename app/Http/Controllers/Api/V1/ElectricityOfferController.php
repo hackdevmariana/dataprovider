a619b547b925527c3a7adc54f52cd04437d96acc
Fernando <fernando@juandemariana.org>
2025-08-19 07:45:30 +0200

    Add ElectricityOffer controller.

---
+++ app/Http/Controllers/Api/V1/ElectricityOfferController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\ElectricityOffer;
+use Illuminate\Http\Request;
+
+class ElectricityOfferController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ElectricityOffer $electricityOffer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ElectricityOffer $electricityOffer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ElectricityOffer $electricityOffer)
+    {
+        //
+    }
+}
