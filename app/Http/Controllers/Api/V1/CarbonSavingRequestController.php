20854f72b75021c2b3c592de02c1f46216cd1b8b
Fernando <fernando@juandemariana.org>
2025-08-19 07:40:46 +0200

    Add CarbonSavingRequest controller.

---
+++ app/Http/Controllers/Api/V1/CarbonSavingRequestController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\CarbonSavingRequest;
+use Illuminate\Http\Request;
+
+class CarbonSavingRequestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CarbonSavingRequest $carbonSavingRequest)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, CarbonSavingRequest $carbonSavingRequest)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CarbonSavingRequest $carbonSavingRequest)
+    {
+        //
+    }
+}
