0f08dc7dbc07a094fcdb093e25100625e61fba30
Fernando <fernando@juandemariana.org>
2025-08-19 07:43:55 +0200

    Add CooperativeUserMember controller.

---
+++ app/Http/Controllers/Api/V1/CooperativeUserMemberController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\CooperativeUserMember;
+use Illuminate\Http\Request;
+
+class CooperativeUserMemberController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CooperativeUserMember $cooperativeUserMember)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, CooperativeUserMember $cooperativeUserMember)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CooperativeUserMember $cooperativeUserMember)
+    {
+        //
+    }
+}
