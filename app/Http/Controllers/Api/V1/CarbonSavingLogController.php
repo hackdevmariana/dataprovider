9116fb1210ae86558998927d1523adb89929d71b
Fernando <fernando@juandemariana.org>
2025-08-19 07:41:01 +0200

    Add CarbonSavingLog controller.

---
+++ app/Http/Controllers/Api/V1/CarbonSavingLogController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\CarbonSavingLog;
+use Illuminate\Http\Request;
+
+class CarbonSavingLogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CarbonSavingLog $carbonSavingLog)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, CarbonSavingLog $carbonSavingLog)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CarbonSavingLog $carbonSavingLog)
+    {
+        //
+    }
+}
