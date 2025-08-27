e90b25337f83d2c86c7996f36d1340ecf0ae955e
Fernando <fernando@juandemariana.org>
2025-08-27 22:15:29 +0200

    Add Timeline Filament resource.

---
+++ app/Filament/Resources/TimelineResource.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\TimelineResource\Pages;
+use App\Filament\Resources\TimelineResource\RelationManagers;
+use App\Models\Timeline;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class TimelineResource extends Resource
+{
+    protected static ?string $model = Timeline::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('title')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Textarea::make('description')
+                    ->columnSpanFull(),
+                Forms\Components\TextInput::make('theme')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\DatePicker::make('start_date')
+                    ->required(),
+                Forms\Components\DatePicker::make('end_date'),
+                Forms\Components\TextInput::make('events'),
+                Forms\Components\TextInput::make('view_type')
+                    ->required()
+                    ->maxLength(255)
+                    ->default('chronological'),
+                Forms\Components\TextInput::make('categories'),
+                Forms\Components\Toggle::make('is_public')
+                    ->required(),
+                Forms\Components\TextInput::make('created_by')
+                    ->numeric(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('title')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('theme')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('start_date')
+                    ->date()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('end_date')
+                    ->date()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('view_type')
+                    ->searchable(),
+                Tables\Columns\IconColumn::make('is_public')
+                    ->boolean(),
+                Tables\Columns\TextColumn::make('created_by')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTimelines::route('/'),
+            'create' => Pages\CreateTimeline::route('/create'),
+            'edit' => Pages\EditTimeline::route('/{record}/edit'),
+        ];
+    }
+}
