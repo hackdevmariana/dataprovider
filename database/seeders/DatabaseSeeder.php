4894b94ebb3a22fb2c6906d921e24c7470fa3b1f
Fernando <fernando@juandemariana.org>
2025-08-28 18:09:19 +0200

    Fix RealTimePrice seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,132 +11,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-
         $this->call([
-            // Core system data
-            RolesAndAdminSeeder::class,
-            UserRolesSeeder::class,
             UserSeeder::class,
-            AppSettingSeeder::class,
-            
-            // Reference data - Geographic
-            TimezoneSeeder::class,
-            LanguageSeeder::class,
-            CountrySeeder::class,
-            AutonomousCommunitySeeder::class,
-            ProvinceSeeder::class,
-            RegionSeeder::class,
-            \Database\Seeders\Municipality\MadridMunicipalitiesSeeder::class,
-            
-            // Reference data - Economic
-            CurrencySeeder::class,
-            PriceUnitSeeder::class,
-            CompanyTypeSeeder::class,
-            
-            // Reference data - Cultural
-            EventTypeSeeder::class,
-            VenueTypeSeeder::class,
-            TagSeeder::class,
-            
-            // Reference data - People
-            ProfessionSeeder::class,
-            PersonSeeder::class,
-            RelationshipTypeSeeder::class,
-            FamilyMemberSeeder::class,
-            AliasSeeder::class,
-            AppearanceSeeder::class,
-            PersonProfessionSeeder::class,
-            CooperativeUserMemberSeeder::class,
-            
-            // Cultural and artistic data
-            ArtistSeeder::class,
-            GroupSeeder::class,
-            SpanishRapSeeder::class,
-            EventSeeder::class,
-            CalendarHolidayLocationSeeder::class,
-            AwardSeeder::class,
-            AnniversarySeeder::class,
-            CalendarHolidaySeeder::class,
-            CatholicSaintSeeder::class,
-            
-            // Energy sector data
-            EnergyCompanySeeder::class,
-            ElectricityPriceSeeder::class,
-            ElectricityPriceIntervalSeeder::class,
-            ElectricityOfferSeeder::class,
-            EnergyInstallationSeeder::class,
-            EnergyTransactionSeeder::class,
-            ProductionRightSeeder::class,
-            CooperativeSeeder::class,
-            ExchangeRateSeeder::class,
-            
-            // Geographic data
-            PointOfInterestSeeder::class,
-            ZoneClimateSeeder::class,
-            
-            // Sustainability data
-            CarbonEquivalenceSeeder::class,
-            CarbonCalculationSeeder::class,
-            CarbonSavingRequestSeeder::class,
-            CarbonSavingLogSeeder::class,
-            EmissionFactorSeeder::class,
-            PlantSpeciesSeeder::class,
-            EnergyCertificateSeeder::class,
-            WeatherAndSolarDataSeeder::class,
-            
-            // Media and communication data
-            MediaOutletSeeder::class,
-            MediaContactSeeder::class,
-            ScrapingSourceSeeder::class,
-            ContentHashtagSeeder::class,
-            
-            // Technology and analytics data
-            UserDeviceSeeder::class,
-            NotificationSettingSeeder::class,
-            StatSeeder::class,
-            ApiKeySeeder::class,
-            
-            // Project and business data
-            ProjectVerificationSeeder::class,
-            ConsultationServiceSeeder::class,
-            TopicMembershipSeeder::class,
-            UserPrivilegeSeeder::class,
-            ExpertVerificationSeeder::class,
-            CooperativePostSeeder::class,
-            SocialComparisonSeeder::class,
-            LeaderboardSeeder::class,
-            UserBookmarkSeeder::class,
-            UserAchievementSeeder::class,
-            UserChallengeSeeder::class,
-            ActivityFeedSeeder::class,
-            CategorySeeder::class,
-            SponsoredContentSeeder::class,
-            
-            // Reputation and gamification data
-            ReputationTransactionSeeder::class,
-            ContentVoteSeeder::class,
-            
-            // Visual identity and branding data
-            ColorSeeder::class,
-            FontSeeder::class,
-            VisualIdentitySeeder::class,
-            TagGroupSeeder::class,
-            
-            // Gamification data
-            AchievementSeeder::class,
-            ChallengeSeeder::class,
-            NewsArticleSeeder::class,
-            UserGeneratedContentSeeder::class,
-            
-            // Topic and discussion data
-            TopicPostSeeder::class,
             TopicCommentSeeder::class,
+            UserGeneratedContentSeeder::class,
+            GroupSeeder::class,
+            FestivalProgramSeeder::class,
+            QuoteSeeder::class,
+            RealTimePriceSeeder::class,
+            NewsSourceSeeder::class,
         ]);
     }
 }
