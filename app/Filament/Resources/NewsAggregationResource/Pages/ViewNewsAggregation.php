39ee7edb79522bf6b57c861dfe764e27ae1a80ed
Fernando <fernando@juandemariana.org>
2025-08-28 13:08:45 +0200

    Add view to NewsAggregation Filament resource.

---
+++ app/Filament/Resources/NewsAggregationResource/Pages/ViewNewsAggregation.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Filament\Resources\NewsAggregationResource\Pages;
+
+use App\Filament\Resources\NewsAggregationResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewNewsAggregation extends ViewRecord
+{
+    protected static string $resource = NewsAggregationResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\EditAction::make()
+                ->label('Editar')
+                ->icon('fas-edit'),
+        ];
+    }
+}
