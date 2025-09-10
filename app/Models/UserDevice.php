475ab10ed4bbeaa3bef9b4c151fd32f9cea2ceea
Fernando <fernando@juandemariana.org>
2025-09-10 10:28:56 +0200

    Fix UserDevice model.

--- app/Models/UserDevice.php
+++ app/Models/UserDevice.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class UserDevice extends Model
 {
@@ -20,7 +21,12 @@ class UserDevice extends Model
         'notifications_enabled',
     ];
 
-    public function user()
+    protected $casts = [
+        'notifications_enabled' => 'boolean',
+    ];
+
+    // Relaciones
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
