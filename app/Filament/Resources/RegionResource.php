4e671e46548dddba4112e992da9e6c8f219a5f06
Fernando <fernando@juandemariana.org>
2025-09-11 21:36:47 +0200

    Add a badge to the Region Filament resource with the number of items.

--- app/Filament/Resources/RegionResource.php
+++ app/Filament/Resources/RegionResource.php
@@ -24,7 +24,11 @@ class RegionResource extends Resource
     protected static ?string $navigationIcon = 'heroicon-o-map-pin';
     protected static ?string $navigationGroup = 'Locations';
 
-    public static function form(Form $form): Form
+    
+    public static function getNavigationBadge(): ?string
+    {
+        return static::getModel()::count();
+    }public static function form(Form $form): Form
     {
         return $form->schema([
             TextInput::make('name')->required()->maxLength(255),
