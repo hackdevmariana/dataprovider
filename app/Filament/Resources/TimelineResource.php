22a14da3b6e4b55f78f114a5c8194f913e45c3e0
Fernando <fernando@juandemariana.org>
2025-09-11 21:40:07 +0200

    Add a badge to the Timeline Filament resource with the number of items.

--- app/Filament/Resources/TimelineResource.php
+++ app/Filament/Resources/TimelineResource.php
@@ -29,7 +29,11 @@ class TimelineResource extends Resource
 
     protected static ?string $pluralModelLabel = 'Líneas de Tiempo';
 
-    public static function form(Form $form): Form
+    
+    public static function getNavigationBadge(): ?string
+    {
+        return static::getModel()::count();
+    }public static function form(Form $form): Form
     {
         return $form
             ->schema([
