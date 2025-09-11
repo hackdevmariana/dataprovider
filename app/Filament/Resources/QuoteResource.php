3aea20a54969df939b3b082bfdb4674e66f3ceee
Fernando <fernando@juandemariana.org>
2025-09-11 21:32:34 +0200

    Add a badge to the Quote Filament resource with the number of items.

--- app/Filament/Resources/QuoteResource.php
+++ app/Filament/Resources/QuoteResource.php
@@ -29,7 +29,11 @@ class QuoteResource extends Resource
 
     protected static ?string $pluralModelLabel = 'Citas';
 
-    public static function form(Form $form): Form
+    
+    public static function getNavigationBadge(): ?string
+    {
+        return static::getModel()::count();
+    }public static function form(Form $form): Form
     {
         return $form
             ->schema([
