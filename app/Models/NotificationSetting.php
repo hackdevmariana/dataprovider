9d7d51bc3f8554f38ce01970411c3dba59547b63
Fernando <fernando@juandemariana.org>
2025-09-11 10:34:48 +0200

    Add NotificationSetting model.

--- app/Models/NotificationSetting.php
+++ app/Models/NotificationSetting.php
@@ -1,11 +1,14 @@
 <?php
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class NotificationSetting extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'user_id',
         'type',
