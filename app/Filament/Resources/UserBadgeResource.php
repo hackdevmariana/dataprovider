5cb90459c17f7542f9785c9ba1239a20b3cfa092
Fernando <fernando@juandemariana.org>
2025-09-11 21:27:53 +0200

    Add a badge to the UserBadge Filament resource with the number of items.

--- app/Filament/Resources/UserBadgeResource.php
+++ app/Filament/Resources/UserBadgeResource.php
@@ -26,7 +26,11 @@ class UserBadgeResource extends Resource
 
     protected static ?int $navigationSort = 1;
 
-    public static function form(Form $form): Form
+    
+    public static function getNavigationBadge(): ?string
+    {
+        return static::getModel()::count();
+    }public static function form(Form $form): Form
     {
         return $form
             ->schema([
