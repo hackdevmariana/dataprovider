00ae438fbbaac2c33c9c7b58129e888354f2b774
Fernando <fernando@juandemariana.org>
2025-09-11 21:36:29 +0200

    Add a badge to the Province Filament resource with the number of items.

--- app/Filament/Resources/ProvinceResource.php
+++ app/Filament/Resources/ProvinceResource.php
@@ -24,7 +24,11 @@ class ProvinceResource extends Resource
     protected static ?string $navigationGroup = 'Locations';
 
 
-    public static function form(Form $form): Form
+    
+    public static function getNavigationBadge(): ?string
+    {
+        return static::getModel()::count();
+    }public static function form(Form $form): Form
     {
         return $form->schema([
             TextInput::make('name')->required()->maxLength(255),
