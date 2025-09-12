41e34dc4ba96029bc2f0f74f907972ce901a51d8
Fernando <fernando@juandemariana.org>
2025-09-12 23:27:49 +0200

    Add a badge to the Currency Filament resource with the number of items.

--- app/Filament/Resources/CurrencyResource.php
+++ app/Filament/Resources/CurrencyResource.php
@@ -27,7 +27,11 @@ class CurrencyResource extends Resource
     
     protected static ?string $pluralModelLabel = 'Monedas';
 
-    public static function form(Form $form): Form
+    
+    public static function getNavigationBadge(): ?string
+    {
+        return static::getModel()::count();
+    }public static function form(Form $form): Form
     {
         return $form
             ->schema([
