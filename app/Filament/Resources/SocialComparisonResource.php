7c218cc4da3dbc940816cfa92ff2337760610d32
Fernando <fernando@juandemariana.org>
2025-09-12 22:34:30 +0200

    Add a badge to the SocialComparison Filament resource with the number of items.

--- app/Filament/Resources/SocialComparisonResource.php
+++ app/Filament/Resources/SocialComparisonResource.php
@@ -24,7 +24,11 @@ class SocialComparisonResource extends Resource
 
     protected static ?int $navigationSort = 6;
 
-    public static function form(Form $form): Form
+    
+    public static function getNavigationBadge(): ?string
+    {
+        return static::getModel()::count();
+    }public static function form(Form $form): Form
     {
         return $form
             ->schema([
