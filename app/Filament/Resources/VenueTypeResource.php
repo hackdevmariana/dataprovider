6b3db0941291080c68fe721654d5d1398501d1ab
Fernando <fernando@juandemariana.org>
2025-09-12 23:37:01 +0200

    Add a badge to the VenueType Filament resource with the number of items.

--- app/Filament/Resources/VenueTypeResource.php
+++ app/Filament/Resources/VenueTypeResource.php
@@ -23,7 +23,11 @@ class VenueTypeResource extends Resource
 
     protected static ?string $label = 'Venue Type';
     protected static ?string $pluralLabel = 'Venue Types';
-    public static function form(Form $form): Form
+    
+    public static function getNavigationBadge(): ?string
+    {
+        return static::getModel()::count();
+    }public static function form(Form $form): Form
     {
         return $form->schema([
             Forms\Components\TextInput::make('name')
