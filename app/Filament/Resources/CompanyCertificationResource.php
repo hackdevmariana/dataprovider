09d4960bf2b4ddfbb8313ab5676365256edd68b3
Fernando <fernando@juandemariana.org>
2025-09-12 21:57:58 +0200

    Add a badge to the CompanyCertification Filament resource with the number of items.

--- app/Filament/Resources/CompanyCertificationResource.php
+++ app/Filament/Resources/CompanyCertificationResource.php
@@ -29,7 +29,11 @@ class CompanyCertificationResource extends Resource
 
     protected static ?string $pluralModelLabel = 'Certificaciones Empresariales';
 
-    public static function form(Form $form): Form
+    
+    public static function getNavigationBadge(): ?string
+    {
+        return static::getModel()::count();
+    }public static function form(Form $form): Form
     {
         return $form
             ->schema([
