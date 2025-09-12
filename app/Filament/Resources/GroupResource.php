88b6256baba4ea6501c09e980d6e8673868f2af7
Fernando <fernando@juandemariana.org>
2025-09-12 22:18:40 +0200

    Add a badge to the Group Filament resource with the number of items.

--- app/Filament/Resources/GroupResource.php
+++ app/Filament/Resources/GroupResource.php
@@ -28,7 +28,11 @@ class GroupResource extends Resource
 
 
 
-    public static function form(Form $form): Form
+    
+    public static function getNavigationBadge(): ?string
+    {
+        return static::getModel()::count();
+    }public static function form(Form $form): Form
     {
         return $form->schema([
             Forms\Components\Section::make('Información Básica')
