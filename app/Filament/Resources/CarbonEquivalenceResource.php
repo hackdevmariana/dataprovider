ab8aedb3a250a8b65c2df293fc91e60cf27b6d83
Fernando <fernando@juandemariana.org>
2025-09-12 21:54:32 +0200

    Add a badge to the CarbonEquivalence Filament resource with the number of items.

--- app/Filament/Resources/CarbonEquivalenceResource.php
+++ app/Filament/Resources/CarbonEquivalenceResource.php
@@ -19,7 +19,11 @@ class CarbonEquivalenceResource extends Resource
     protected static ?string $navigationGroup = 'Energy & Environment';
 
     protected static ?string $navigationIcon = 'heroicon-o-scale';
-    public static function form(Form $form): Form
+    
+    public static function getNavigationBadge(): ?string
+    {
+        return static::getModel()::count();
+    }public static function form(Form $form): Form
     {
         return $form->schema([
             Forms\Components\TextInput::make('name')->required()->maxLength(255),
