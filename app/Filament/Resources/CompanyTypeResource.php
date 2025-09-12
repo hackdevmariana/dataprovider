8a74b1094dfbb1ace8cfd4af1fa2efd75b62ffba
Fernando <fernando@juandemariana.org>
2025-09-12 21:57:45 +0200

    Add a badge to the Company Filament resource with the number of items.

--- app/Filament/Resources/CompanyTypeResource.php
+++ app/Filament/Resources/CompanyTypeResource.php
@@ -20,6 +20,11 @@ class CompanyTypeResource extends Resource
     protected static ?string $navigationIcon = 'heroicon-o-building-office';
     protected static ?string $navigationGroup = 'People & Organizations';
 
+    public static function getNavigationBadge(): ?string
+    {
+        return static::getModel()::count();
+    }
+
 
     public static function form(Form $form): Form
     {
