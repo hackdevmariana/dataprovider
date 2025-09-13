8187a357d5c73ca511750a2f07cd5e094385c3d8
Fernando <fernando@juandemariana.org>
2025-09-13 14:14:43 +0200

    Fix SocialAccount model.

--- app/Models/SocialAccount.php
+++ app/Models/SocialAccount.php
@@ -3,14 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class SocialAccount extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'person_id',
         'platform',
         'handle',
         'url',
+        'followers_count',
+        'verified',
+        'is_public',
+    ];
+
+    protected $casts = [
+        'verified' => 'boolean',
+        'is_public' => 'boolean',
     ];
 
     public function person()
