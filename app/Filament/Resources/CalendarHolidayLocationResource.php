1079bda4df4b035f7ad3439056bf42e03e9186c2
Fernando <fernando@juandemariana.org>
2025-09-13 14:08:04 +0200

    Add a badge to the CalendarHolidayLocation Filament resource with the number of items.

--- app/Filament/Resources/CalendarHolidayLocationResource.php
+++ app/Filament/Resources/CalendarHolidayLocationResource.php
@@ -27,7 +27,11 @@ class CalendarHolidayLocationResource extends Resource
     
     protected static ?string $pluralModelLabel = 'Ubicaciones de Feriados';
 
-    public static function form(Form $form): Form
+    
+    public static function getNavigationBadge(): ?string
+    {
+        return static::getModel()::count();
+    }public static function form(Form $form): Form
     {
         return $form
             ->schema([
