d74d4fae30766c770c8ee706ca7006312d7d053b
Fernando <fernando@juandemariana.org>
2025-09-20 12:49:05 +0200

    Fix Anniversary Filament resource.

--- app/Filament/Resources/AnniversaryResource.php
+++ app/Filament/Resources/AnniversaryResource.php
@@ -17,7 +17,7 @@ class AnniversaryResource extends Resource
 {
     protected static ?string $model = Anniversary::class;
 
-    protected static ?string $navigationGroup = 'Events & Calendar';
+    protected static ?string $navigationGroup = 'Eventos y Cultura';
     protected static ?string $navigationIcon = 'heroicon-o-calendar';
     
     public static function getNavigationBadge(): ?string
