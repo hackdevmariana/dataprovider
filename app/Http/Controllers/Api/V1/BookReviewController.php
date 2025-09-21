907601e9ff4f9af16771c8b659a57e62f2fb5ce1
Fernando <fernando@juandemariana.org>
2025-09-21 22:25:14 +0200

    Add BookReview controller.

---
+++ app/Http/Controllers/Api/V1/BookReviewController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+
+class BookReviewController extends Controller
+{
+    public function index(Request $request): JsonResponse
+    {
+        return response()->json([
+            "data" => [],
+            "message" => "Endpoint en desarrollo"
+        ]);
+    }
+
+    public function store(Request $request): JsonResponse
+    {
+        return response()->json([
+            "message" => "Endpoint en desarrollo"
+        ], 501);
+    }
+
+    public function show(string $id): JsonResponse
+    {
+        return response()->json([
+            "data" => null,
+            "message" => "Endpoint en desarrollo"
+        ]);
+    }
+
+    public function update(Request $request, string $id): JsonResponse
+    {
+        return response()->json([
+            "message" => "Endpoint en desarrollo"
+        ], 501);
+    }
+
+    public function destroy(string $id): JsonResponse
+    {
+        return response()->json([
+            "message" => "Endpoint en desarrollo"
+        ], 501);
+    }
+}
