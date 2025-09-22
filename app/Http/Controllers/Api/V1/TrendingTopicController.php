99f524f770e626bb824e660b9b81da40929948b8
Fernando <fernando@juandemariana.org>
2025-09-22 17:24:38 +0200

    Add documentation to TrendingTopic controller.

--- app/Http/Controllers/Api/V1/TrendingTopicController.php
+++ app/Http/Controllers/Api/V1/TrendingTopicController.php
@@ -5,7 +5,14 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
+use OpenApi\Annotations as OA;
 
+/**
+ * @OA\Tag(
+ *     name="Temas Trending",
+ *     description="APIs para la gestión de Temas Trending"
+ * )
+ */
 class TrendingTopicController extends Controller
 {
     public function index(Request $request): JsonResponse
