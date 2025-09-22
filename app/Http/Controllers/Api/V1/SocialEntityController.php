f72a5dd342c9fc5fa44eadebede5ac3cc0ef8109
Fernando <fernando@juandemariana.org>
2025-09-22 17:24:36 +0200

    Add documentation to SocialEntity controller.

--- app/Http/Controllers/Api/V1/SocialEntityController.php
+++ app/Http/Controllers/Api/V1/SocialEntityController.php
@@ -5,7 +5,14 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
+use OpenApi\Annotations as OA;
 
+/**
+ * @OA\Tag(
+ *     name="Entidades Sociales",
+ *     description="APIs para la gestión de Entidades Sociales"
+ * )
+ */
 class SocialEntityController extends Controller
 {
     public function index(Request $request): JsonResponse
