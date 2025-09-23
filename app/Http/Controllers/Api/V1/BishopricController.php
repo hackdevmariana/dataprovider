f0fe2b5646addaa677397e95e58b0e1248f5cb98
Fernando <fernando@juandemariana.org>
2025-09-23 16:30:05 +0200

    Add documentation to Bishopric controller.

--- app/Http/Controllers/Api/V1/BishopricController.php
+++ app/Http/Controllers/Api/V1/BishopricController.php
@@ -5,14 +5,6 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
-use OpenApi\Annotations as OA;
-
-/**
- * @OA\Tag(
- *     name="Obispados",
- *     description="APIs para la gestión de Obispados"
- * )
- */
 class BishopricController extends Controller
 {
     public function index(Request $request): JsonResponse
