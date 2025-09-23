d926fb100b0b0a25cf705c05230cace2b58aa7b9
Fernando <fernando@juandemariana.org>
2025-09-23 16:30:04 +0200

    Add documentation to Timeline controller.

--- app/Http/Controllers/Api/V1/TimelineController.php
+++ app/Http/Controllers/Api/V1/TimelineController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
-
 class TimelineController extends Controller
 {
     public function index(Request $request): JsonResponse
