cda31fa5d131b79fd03bb62224d62c9b0b502d31
Fernando <fernando@juandemariana.org>
2025-09-23 16:30:04 +0200

    Add documentation to FestivalProgram controller.

--- app/Http/Controllers/Api/V1/FestivalProgramController.php
+++ app/Http/Controllers/Api/V1/FestivalProgramController.php
@@ -5,14 +5,6 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
-use OpenApi\Annotations as OA;
-
-/**
- * @OA\Tag(
- *     name="Programas de Festivales",
- *     description="APIs para la gestión de Programas de Festivales"
- * )
- */
 class FestivalProgramController extends Controller
 {
     public function index(Request $request): JsonResponse
