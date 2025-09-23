d83145890d60f45aa2fd581d589c770e493298c6
Fernando <fernando@juandemariana.org>
2025-09-23 16:30:03 +0200

    Add documentation to CooperativePost controller.

--- app/Http/Controllers/Api/V1/CooperativePostController.php
+++ app/Http/Controllers/Api/V1/CooperativePostController.php
@@ -14,12 +14,6 @@ use Illuminate\Support\Facades\Validator;
  * 
  * API endpoints for managing cooperative posts and announcements.
  */
-/**
- * @OA\Tag(
- *     name="Posts de Cooperativas",
- *     description="APIs para la gestión de Posts de Cooperativas"
- * )
- */
 class CooperativePostController extends Controller
 {
     /**
