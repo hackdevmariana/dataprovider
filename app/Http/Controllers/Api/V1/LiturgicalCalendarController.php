c37e7b12447fad39e9d7c2cfa47c33e8a584df6a
Fernando <fernando@juandemariana.org>
2025-09-24 19:59:26 +0200

    Add documentation to LiturgicalCalendar controller.

--- app/Http/Controllers/Api/V1/LiturgicalCalendarController.php
+++ app/Http/Controllers/Api/V1/LiturgicalCalendarController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
-
 class LiturgicalCalendarController extends Controller
 {
     public function index(Request $request): JsonResponse
